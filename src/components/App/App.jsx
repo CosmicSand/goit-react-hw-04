a6d6c74171c0ad86f936a658fda19652ab4354c2
CosmicSand <skyjlen@gmail.com>
2024-03-10 23:44:54 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,9 @@
 import css from "./App.module.css";
 
-function App() {}
+function App() {
+  const appId = 577372;
+  const accessKey = "MgGVu-2Aj7GbcWHyEAULVPxtWi0-9yK_brGw5GgXLKI";
+  const securityKey = "0Bi-McYmSz35ROYe7Vcwkh3cNuZnzS2E91IQZu5IUms";
+}
 
 export default App;
