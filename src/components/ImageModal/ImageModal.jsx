c5e50ff6cfc98e96097e1ed066ac58859dab9668
CosmicSand <skyjlen@gmail.com>
2024-03-13 04:37:35 +0200

    upd

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -22,8 +22,14 @@ function ImageModal({ value: { url, alt, name, location, portfolio } }) {
           <img src={url} alt={alt} />
           <p className={css.text}>
             {name}
-            {location && ` from ${location}`} --&gt; More cool photos
-            <a href={portfolio} target="_blank" rel="noreferrer">
+            {location ? ` from ${location}` : ` from lovely Earth planet`}{" "}
+            --&gt; More cool photos
+            <a
+              className={css.link}
+              href={portfolio}
+              target="_blank"
+              rel="noreferrer"
+            >
               &nbsp;here
             </a>
           </p>
