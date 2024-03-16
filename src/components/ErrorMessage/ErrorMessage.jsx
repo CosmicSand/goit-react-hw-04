ae50f91608f0124361196da9d01d0692825a5b6b
CosmicSand <skyjlen@gmail.com>
2024-03-17 01:56:46 +0200

    Update ErrorMessage.jsx

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,7 @@
 import css from "./ErrorMessage.module.css";
 
 function ErrorMessage({ errorObj }) {
-  return (
-    <p className={css.text}>
-      Ooops! {errorObj.message}! Just refresh the page.
-    </p>
-  );
+  return <p className={css.text}>Ooops! {errorObj.message}!</p>;
 }
 
 export default ErrorMessage;
